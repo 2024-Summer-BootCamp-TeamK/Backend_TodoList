4d2cde3a7ba8ef75f477e182f624c47fc9b709c7
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-06-25 01:33:31 +0900

    initial setting

---
+++ myproject/todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class TestView(APIView):
+    def get(self, request):
+        return Response({"message": "hello"}, status=status.HTTP_200_OK)
