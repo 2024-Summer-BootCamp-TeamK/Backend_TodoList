c202d20952aff51204979bc086648bb4a42dcf25
woohyun23 <parkwh0823@gmail.com>
2024-06-25 20:50:49 +0900

    feat: todo수정 삭제 기능

--- myproject/todo/urls.py
+++ myproject/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ToDoAPIView
+from .views import ToDoAPIView, ToDoManageView
 
 urlpatterns = [
     path('todos/', ToDoAPIView.as_view(), name='todo_list'),
+    path('todos/<int:id>', ToDoManageView.as_view(), name='todo_list_update'),
 ]
--- myproject/todo/views.py
+++ myproject/todo/views.py
@@ -28,3 +28,39 @@ class ToDoAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ToDoManageView(APIView):
+    @swagger_auto_schema(
+        request_body=ToDoSerializer,
+        manual_parameters=[
+            openapi.Parameter('id', openapi.IN_PATH, description="ID of the ToDo item to update",
+                              type=openapi.TYPE_INTEGER)
+        ],
+        responses={
+            200: openapi.Response('OK', ToDoSerializer),
+            400: 'Bad Request',
+            404: 'Not Found'
+        }
+    )
+    def put(self, request, id):
+        todo = get_object_or_404(ToDo, pk=id)
+        serializer = ToDoSerializer(todo, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('id', openapi.IN_PATH, description="ID of the ToDo item to delete",
+                              type=openapi.TYPE_INTEGER)
+        ],
+        responses={
+            204: 'No Content',
+            404: 'Not Found'
+        }
+    )
+    def delete(self, request, id):
+        todo = get_object_or_404(ToDo, pk=id)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
