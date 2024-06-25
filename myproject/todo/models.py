8a3a5936a28f10638115964427e1738933ec6b52
woohyun23 <parkwh0823@gmail.com>
2024-06-25 15:17:39 +0900

    feat: todo 모델 생성

--- myproject/todo/models.py
+++ myproject/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length = 200)
+    due_date = models.DateTimeField(null = True, blank = True)
+
+    def __str__(self):
+        return self.title
