c53ddaf22d4df3afd6f141a562ac3bdd50f73ec5
woohyun23 <parkwh0823@gmail.com>
2024-06-26 16:30:37 +0900

    feat: 경로 설정

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', include('todo.urls')),
+    path('api/', include('todo.urls')),
 ]
